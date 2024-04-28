821a96849f8677d51f15f7c4f1967521cddf3127
YashBhamare32 <bobbybhamare32@gmail.com>
2024-04-29 01:47:54 +0530

    dark mode tried

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Navbar } from "./Navbar";
 
 export function About(){
@@ -15,10 +16,12 @@ export function About(){
                 <span className="text-xl ">This word counter software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count characters in facebook, blog, books, excel document, pdf document, essays, etc.</span>
             </div>
 
-            <div className="my-8">
+            <div className="my-8 mb-16">
                 <div className="text-3xl font-semibold">Analyze your text</div>
                 <span className="text-xl ">TextEdit gives you a way to analyze your text quickly and efficiently. Be it word count, character count or</span>
             </div>
+
+            <Link to="/" className="bg-indigo-300 p-4 mt-8 text-xl">Back to home page</Link>
         </div>
     )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export function Navbar() {
+  const [dark , setDark] = useState(false);
+  function handleDark(){
+    setDark(!dark);
+  }
   return (
-    <div className="p-4 flex items-center">
-      <Link to="/" className="font-bold text-5xl text-indigo-400 mr-2">TextEdit</Link>
-      <Link to="/home" className="font-bold text-xl mx-2">Home</Link>
-      <Link to="/about" className="font-bold text-xl mx-2">About</Link>
+    <div className="p-4 flex  justify-between">
+      <div className=" flex items-center">
+        <Link to="/" className="font-bold text-5xl text-indigo-400 mr-2">TextEdit</Link>
+        <Link to="/" className="font-bold text-xl mx-2">Home</Link>
+        <Link to="/about" className="font-bold text-xl mx-2">About</Link>
+      </div>
+      {/* <div className=" flex items-center bg-indigo-300 p-2 rounded-md">
+        <button onClick={handleDark}>Enable dark mode</button>
+      </div> */}
     </div>
   );
 }
